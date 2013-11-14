4ed26d2c9a155a2299a3e86e0ee46be8171b34bf
Eric Duvon <eric@gromobi.com>
2013-11-14 00:20:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7d755d7bc1f9944e94fa259991e2abcbf39ab38708e7a61c49a9de29dffe8268a5124360682278c7f89a726903ce3589e35f4d4722855d3e11fb0f60b3b58f1a'
